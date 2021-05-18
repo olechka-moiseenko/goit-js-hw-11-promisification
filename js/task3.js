24ce4dfe0061f3f9fe7af0adeaf409efb6f07a4a
olechka.moiseenko <olechka.moiseenko@gmail.com>
2021-05-18 13:19:31 +0300

    Аргумент resolve объект

--- js/task3.js
+++ js/task3.js
@@ -41,10 +41,11 @@
   const makeTransaction = (transaction) => {
    return new Promise((resolve, reject) => {
     const delay = randomIntegerFromInterval(200, 500);
+    console.log(delay);
         setTimeout(() => {
           const canProcess = Math.random() > 0.3;
           if (canProcess) {
-              resolve (transaction.id, delay);
+              resolve ({id: transaction.id, time: delay});
           }
           else {
               reject (transaction.id);
@@ -53,7 +54,7 @@
    });
   };
 
-    const logSuccess = (id, time) => {
+  const logSuccess = ({id, time}) => {
     console.log(`Transaction ${id} processed in ${time}ms`);
   };
   
